421f24dc14872f06a13d0c8ea0320b29fbe952e6
adhamlucas <adhamlucas20@gmail.com>
2025-02-19 21:57:56 -0400

    feat: add logger middleware, error handling example, pipes for endpoints parameters and body validation

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,24 +1,52 @@
-import { Controller, Delete, Get, Put, Post } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Put,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { SongsService } from './songs.service';
+import { CreateSongDTO } from './dto/create-song-dto';
 
 @Controller('songs')
 export class SongsController {
   constructor(private songsService: SongsService) {}
 
   @Post()
-  create() {
-    this.songsService.create('Animals by Martin Garrix');
+  create(@Body() createSongDTO: CreateSongDTO) {
+    this.songsService.create(createSongDTO);
     return 'create a new song endpoint';
   }
 
   @Get()
   findAll() {
-    return this.songsService.findAll();
+    try {
+      return this.songsService.findAll();
+    } catch (error) {
+      throw new HttpException(
+        'server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: error,
+        },
+      );
+    }
   }
 
   @Get(':id')
-  findOne() {
-    return 'fetch song based on id';
+  findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
+    return `fetch song based on id ${typeof id}`;
   }
 
   @Put(':id')
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -8,11 +8,15 @@ export class SongsService {
   private readonly songs: Array<string> = [];
 
   create(song) {
+    // save this song in the database
     this.songs.push(song);
     return this.songs;
   }
 
   findAll() {
+    // fetch the songs from the db
+    // Erros come while fetching the data from DB
+    throw new Error('Error in Db while fetching record');
     return this.songs;
   }
 }
