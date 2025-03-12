e99bf3318c49d77dcaba9f87042399c6f7920c88
adhamlucas <adhamlucas20@gmail.com>
2025-03-12 13:46:07 -0400

    chore(database): first dabatase, using typeorm, settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,31 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 import { SongsModule } from './songs/songs.module';
+import { DevConfigService } from './common/providers/DevConfigService';
 // import { SongsController } from './songs/songs.controller';
+import { DatabaseModule } from './database/database.module';
+
+const devConfig = { port: 3000 };
+const proConfig = { port: 400 };
 
 @Module({
-  imports: [SongsModule],
+  imports: [SongsModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: DevConfigService,
+      useClass: DevConfigService,
+    },
+    {
+      provide: 'CONFIG',
+      useFactory: () => {
+        return process.env.NODE_ENV === 'development' ? devConfig : proConfig;
+      },
+    },
+  ],
 })
+
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('songs'); // option number 1
